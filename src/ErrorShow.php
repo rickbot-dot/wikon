104751d14925a61e90ebb9b59d9b74404d69168c
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-07-30 13:37:57 +0100

    Update ErrorShow.php

--- src/ErrorShow.php
+++ src/ErrorShow.php
@@ -1,6 +1,7 @@
 <?php
 $wikon_version = WIKON;
 echo <<<EOF
+<style>body{font-family:Arial,sans-serif;padding:10px}h1{font-weight:normal}</style>
 <h1>Wikon $wikon_version internal error</h1>
 <p>$error</p>
 EOF;
