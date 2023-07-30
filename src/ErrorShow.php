f79d91cc63fa7b8fc5996b149a05d2859bd11532
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-07-30 13:15:04 +0100

    Update ErrorShow.php

--- src/ErrorShow.php
+++ src/ErrorShow.php
@@ -5,4 +5,5 @@ echo <<<EOF
 <p>$error</p>
 EOF;
 header("HTTP/1.1 500 Internal Server Error");
+error_log("[Wikon $wikon_version] " . strip_tags(str_replace(['<br>', '<br />', '<br/>'], ' ', $error));
 exit;
