5264dc574ea8db64ea98bb3d61539568490625da
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-09-28 22:18:31 +0100

    Update Installer.php

--- src/Installer.php
+++ src/Installer.php
@@ -4,8 +4,6 @@ function check_session() {
     return session_status() === PHP_SESSION_ACTIVE;
 }
 
-$session_max_lifetime = ini_get("session.gc_maxlifetime");
-
 if (check_session()) {
     if ($_COOKIE["session_id"] !== $_SESSION['id']) {
         // Redirect or display an error message
@@ -14,7 +12,7 @@ if (check_session()) {
 }
 
 session_start();
-setcookie("session_id", $_SESSION['id'], time() + $session_max_lifetime, "/");
+setcookie("session_id", $_SESSION['id'], time() + 1440, "/");
 
 echo <<<HTML
 
